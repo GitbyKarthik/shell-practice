c0da23e2b7a97d1ee9e7feff6f7baad567e68a3a
karthikmallina <mallinakarthik7298@gmail.com>
2025-11-04 15:01:21 +0530

    session-06

--- 13-installation.sh
+++ 13-installation.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelage"
+    exit 1 # failure is other than 0
+fi
+
+dnf install mysql -y
+ 
+if [$? -ne 0 ]; then
+    echo "EROR:: Installing MySQL is failure"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
+fi
+
+dnf install nginx -y
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelage"
+    exit 1 # failure is other than 0
